2224c38a184dcb38246b05bdea28d4e39f388e11
Anggoro Setyawan <anggoros90@gmail.com>
2022-06-12 19:24:58 +0700

    revisi

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -18,30 +18,67 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = (fnCallback) => {
-  const result = [];
+const fs = require("fs");
 
-  fs.readFile(file1, { encoding: "utf8" }, (err, data) => {
-    const content = JSON.parse(data);
-    message = content.message;
-    result.push(message.split(" ")[1]);
+// ! JANGAN DIMODIFIKASI
+let file1 = "./data1.json";
+let file2 = "./data2.json";
+let file3 = "./data3.json";
 
-    fs.readFile(file2, { encoding: "utf8" }, (err, data) => {
-      const content = JSON.parse(data);
-      message = content[0].message;
-      result.push(message.split(" ")[1]);
-
-      fs.readFile(file3, { encoding: "utf8" }, (err, data) => {
-        const content = JSON.parse(data);
-        message = content[0].data.message;
-        result.push(message.split(" ")[1]);
-
-        // console.log(result);
-        fnCallback(err, result);
-      });
-    });
-  });
+// ! JANGAN DIMODIFIKASI
+let modifyFile1 = (val) => {
+  file1 = val;
 };
+let modifyFile2 = (val) => {
+  file2 = val;
+};
+let modifyFile3 = (val) => {
+  file3 = val;
+};
+
+// TODO: Kerjakan bacaData
+// gunakan variabel file1, file2, dan file3
+const fs = require("fs");
+
+// ! JANGAN DIMODIFIKASI
+let file1 = "./data1.json";
+let file2 = "./data2.json";
+let file3 = "./data3.json";
+
+// ! JANGAN DIMODIFIKASI
+let modifyFile1 = (val) => {
+  file1 = val;
+};
+let modifyFile2 = (val) => {
+  file2 = val;
+};
+let modifyFile3 = (val) => {
+  file3 = val;
+};
+
+// TODO: Kerjakan bacaData
+// gunakan variabel file1, file2, dan file3
+const bacaData = (arg) => {
+
+  const result = [];
+  fs.readFile(file1, 'utf8', function read(err, data) {
+    let content = JSON.parse(data)
+    message = content.message;
+    result.push(message.split(' ')[1])
+    fs.readFile(file2, 'utf8', function read(err, data) {
+      content = JSON.parse(data)
+      message = content[0].message;
+      result.push(message.split(' ')[1])
+      fs.readFile(file3, 'utf8', function read(err, data) {
+        content = JSON.parse(data)
+        message = content[0].data.message;
+        result.push(message.split(' ')[1])
+        console.log(result)
+        arg(err, result)
+      })
+    })
+  })
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
